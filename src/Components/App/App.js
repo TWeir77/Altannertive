f6b7b3ec55c62cae6c89e021c809ac50b18d2503
Tanner <tanner.weir15@gmail.com>
2022-11-29 23:04:51 -0800

    Debug Static Components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,13 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, {name: 'name3', artist: 'artist3', album: 'album3', id: 3}]
+    }
+  }
   render() {
     return (
       <div>
@@ -13,8 +20,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults}/>
+            <Playlist searchResults={this.state.searchResults}/>
           </div>
         </div>
       </div>
